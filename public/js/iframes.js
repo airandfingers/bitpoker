0d13462ae4177c3ad16aea9aeb13a9c8a5d168d3
bobbyinfj <robertnakano@gmail.com>
2013-08-20 00:21:01 -0700

    Table rescaling (WIP).

--- public/js/iframes.js
+++ public/js/iframes.js
@@ -2,23 +2,21 @@
 function resizeWindowJazz(e) {
 	var height = $(window).height();
 	var width = $(window).width();
+	var ratio = _.min([width / 690, height / 480, 1]);
+	var distance_to_shift = (1-ratio)/2;
+	var translate = distance_to_shift/ratio * 100;
+	var scale_ratio = 'scale('+ ratio + ', ' + ratio + ') translate(-'+ translate + '%, -' + translate + '%)';
+	console.log('scale_ratio is', scale_ratio);
+	console.log('translate number is ', translate);
 	console.log('Window resized with dimensions', height, ',', width);
-	$('iframe').css(back)
+	$('iframe').css('transform', scale_ratio);
+
 }
 
 var throttled = _.throttle(resizeWindowJazz, 2000);
 
-	$(function() {
-		$(window).resize(throttled);
-	});
+$(window).resize(throttled);
 
-/*
-   zoom: 0.75;
-    -moz-transform: scale(0.75);
-    -moz-transform-origin: 0 0;
-    -o-transform: scale(0.75);
-    -o-transform-origin: 0 0;
-    -webkit-transform: scale(0.75);
-    -webkit-transform-origin: 0 0;
-
-    */
+$(function() {
+	resizeWindowJazz();
+});
