7e1e06b9f9a03a54e971c823b72076d1cba566ba
w <narp@narp-VirtualBox.(none)>
2013-12-31 13:17:39 +0800

    specified fee of only 0.0001 in routes.js to withdrawing

--- routes.js
+++ routes.js
@@ -575,7 +575,8 @@ module.exports = (function () {
       , url = 'https://blockchain.info/merchant/' + db_config.WALLET_ID + '/payment' +
               '?password=' + db_config.WALLET_PASSWORD +
               '&to=' + withdraw_address +
-              '&amount=' + num_satoshi;
+              '&amount=' + num_satoshi
+              '$fee='0.0001/100000000;
     console.log('num_satoshi:', num_satoshi, 'withdraw_address:', withdraw_address, 'url:', url);
     if (num_satoshi > 0) {
       req.user.checkBalance('satoshi', function(err, balance_in_satoshi) {
