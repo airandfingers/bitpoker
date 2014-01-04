95824ce2ce1ec1aba120bdea6b7cfba62f3fa1aa
w <narp@narp-VirtualBox.(none)>
2014-01-04 14:01:05 +0800

    modified push file

--- push_generic.sh
+++ push_generic.sh
@@ -5,13 +5,13 @@
 
 #run this from inside the bitpoker directory OR
 ##cd Desktop/bitpoker cd to the bitpoker directory if needed
-cp public/js/pokertable.min.js src/pokertable.js
-uglifyjs src/pokertable.js -o public/js/pokertable.min.js
-git add -A
+cp public/js/pokertable.min.js src/pokertable.js -v
+uglifyjs src/pokertable.js -o public/js/pokertable.min.js -v
+git add -A -v
 git commit
 #edit your push command as u like here to include usernames/password
-git push
+git push -v
 #line below if u want to include plaintext username/pass
 #sudo git push --repo https://wyvernmonarch7:derkaderka@github.org/bitpoker.git
-#cp src/pokertable.js public/js/pokertable.min.js
+cp src/pokertable.js public/js/pokertable.min.js -v
 echo 'done'
