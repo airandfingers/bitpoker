590e89ce3bfd6100a613abbcd6eea2aed895186f
w <narp@narp-VirtualBox.(none)>
2014-01-04 15:00:41 +0800

    fetch_generic and push_generic added these shell scripts can be used to push/fetch in 1 click configurable features: no need to enter pass/username will automatically backup and then  minify pokertable.min.js (or other files that u want minified), and tehn copy back the original copy after can check to see if a file is minified first before performing minification functions

--- fetch_generic.sh
+++ fetch_generic.sh
@@ -3,15 +3,29 @@
 #u may have to run chmod +x $thisfilename
 #one time before being able to execute this file
 
+#1. checks to see if pokertable.min.js is minified
+#2. if it is not, it copies to src/pokertable.js, the minifies pokertable.min.js
+#3. git commits local changes
+#4. git fetches from everyone
+#5. git merges with everyone
+#6. copies pokertable.js ---> pokertable.min.js
+
+
 #run this from inside the bitpoker directory OR
 ##cd Desktop/bitpoker cd to the bitpoker directory if needed
 
 #check number of lines to see if it is minified
-lines=$(wc -l public/js/pokertable.min.js)
-if($lines > 200);then
+lines=$(wc -l < public/js/pokertable.min.js)
+echo $lines
+read $gorb
+if(($lines > 300))
+	then
 	#we copy it to src, and minify original
 cp public/js/pokertable.min.js src/pokertable.js -v
+echo 'we are uglifying pokertable.min.js'
 uglifyjs src/pokertable.js -o public/js/pokertable.min.js -v
+else
+	echo 'pokertable.min.js is already minified'
 fi
 
 
@@ -30,6 +44,7 @@ git merge user1/master -v
 git merge user2/master -v
 git merge user3/master -v
 
+cp src/pokertable.js public/js/pokertable.min.js -v
 echo 'press any key then enter to exit: WTB JQUERY'
 read response
 exit
