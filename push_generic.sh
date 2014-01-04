361701e7a5bf8113588a5642be4fdef5a678d29b
w <narp@narp-VirtualBox.(none)>
2014-01-04 14:14:09 +0800

    changed file to .com

--- push_generic.sh
+++ push_generic.sh
@@ -12,7 +12,7 @@ git commit
 #edit your push command as u like here to include usernames/password
 git push -v
 #line below if u want to include plaintext username/pass
-#sudo git push --repo https://wyvernmonarch7:derkaderka@github.org/bitpoker.git
+#git push --repo https://mohammaud:derkaderka@github.com/bitpoker.git
 cp src/pokertable.js public/js/pokertable.min.js -v
 read 'press any key to exit' response
 exit
